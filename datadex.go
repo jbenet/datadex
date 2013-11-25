5394f8ecb8153fbba96fbfeee284a3203ae79c06
Juan Batiz-Benet <juan@benet.ai>
2013-11-25 05:43:59 -0800

    gofmt

--- datadex.go
+++ datadex.go
@@ -1,41 +1,42 @@
 package main
 
 import (
-  "flag"
-  "net/http"
-  "fmt"
-  "log"
-  "os"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 const Version = "0.0.0"
+
 var DEBUG bool
 
 func main() {
 
-  port := flag.Int("port", 6000, "Listen port")
-  vers := flag.Bool("version", false, "Show version")
-  flag.BoolVar(&DEBUG, "debug", false, "Debug mode")
-  flag.Parse()
+	port := flag.Int("port", 6000, "Listen port")
+	vers := flag.Bool("version", false, "Show version")
+	flag.BoolVar(&DEBUG, "debug", false, "Debug mode")
+	flag.Parse()
 
-  if DEBUG {
-    DOut("debugging on\n")
-  }
+	if DEBUG {
+		DOut("debugging on\n")
+	}
 
-  if *vers {
-    DOut("datadex version: %s\n", Version)
-    os.Exit(0)
-  }
+	if *vers {
+		DOut("datadex version: %s\n", Version)
+		os.Exit(0)
+	}
 
-  addr := fmt.Sprintf("localhost:%d", *port)
-  DOut("listening on %s\n", addr)
-  log.Fatal(http.ListenAndServe(addr, nil))
+	addr := fmt.Sprintf("localhost:%d", *port)
+	DOut("listening on %s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
 func DErr(format string, a ...interface{}) {
-  fmt.Fprintf(os.Stderr, format, a...)
+	fmt.Fprintf(os.Stderr, format, a...)
 }
 
 func DOut(format string, a ...interface{}) {
-  fmt.Fprintf(os.Stdout, format, a...)
+	fmt.Fprintf(os.Stdout, format, a...)
 }
