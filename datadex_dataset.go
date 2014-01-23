f9f0558fe63e3d4b49e16cbc4293ecd42c4027e8
Juan Batiz-Benet <juan@benet.ai>
2014-01-23 09:55:16 -0800

    consolidated dataset stuff

--- datadex_dataset.go
+++ datadex_dataset.go
@@ -9,6 +9,54 @@ import (
 	"strings"
 )
 
+
+// This is the object that describes a dataset.
+// It is merely a list of Refs (pointers to manifests), and a list of
+// collaborators allowed to modify the package.
+type Dataset struct {
+	Path    string
+	Name    string
+	Owner   string
+	Tagline string // replicated for convenience. use latest published.
+	Refs    data.DatasetRefs
+
+	// Other users allowed to modify the package.
+	Collaborators map[string]string
+}
+
+func NewDataset(dataset string) *Dataset {
+	parts := strings.Split(dataset, "/")
+	return &Dataset{
+		Path:  dataset,
+		Name:  parts[0],
+		Owner: parts[1],
+	}
+}
+
+func (f *Dataset) Handle() *data.Handle {
+	return data.NewHandle(f.Path)
+}
+
+func (f *Dataset) Valid() bool {
+	return f.Handle().Valid()
+}
+
+func (f *Dataset) UserCanModify(user string) bool {
+	if len(user) == 0 {
+		return false
+	}
+
+	if user == f.Owner {
+		return true
+	}
+
+	_, exists := f.Collaborators[user]
+	return exists
+}
+
+
+// Routes
+
 func dsHomeHandler(w http.ResponseWriter, r *http.Request) {
 	dataset := requestDataset(r)
 	fmt.Fprintf(w, "%s\n", dataset)
